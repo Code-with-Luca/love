2dab6d45569d07e7966645bca294d5b636abccd8
luca400 <alexluca.predoi2018@gmail.com>
2020-07-08 12:08:31 +0300

    changes

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(
+  "Thanks for installing love css framework. Just add it to your style folder and link style.css to your html"
+);
+console.log("================================================");
+console.log("Author: Code with Luca");
